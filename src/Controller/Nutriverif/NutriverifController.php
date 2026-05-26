3f4e65c00586d6030838611378b9310b49e3956c
salaahl <sokhona.salaha@gmail.com>
2026-05-26 23:21:54 +0200

    ajout d'une nouvelle route pour nutriverif

---
+++ src/Controller/Nutriverif/NutriverifController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller\Deepl;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class NutriverifController extends AbstractController
+{
+    private HttpClientInterface $httpClient;
+
+    public function __construct(HttpClientInterface $httpClient)
+    {
+        $this->httpClient = $httpClient;
+    }
+
+    #[Route('/search-products', name: 'search', methods: ['POST'])]
+    public function search(Request $request): JsonResponse
+    {
+        $data = $request->toArray();
+
+        $url = $data['url'] ?? null;
+        $method = strtoupper($data['method'] ?? 'GET');
+
+        if (!isset($url)) {
+            return $this->json(
+                ['error' => 'Le paramètre "url" est requis.'],
+                Response::HTTP_BAD_REQUEST
+            );
+        }
+
+        $response = $this->httpClient->request($method, $url, [
+            'headers' => [
+                'User-Agent' => 'NutriVérif/1.0 (sokhona.salaha@gmail.com)',
+            ],
+        ]);
+
+        if (200 !== $response->getStatusCode()) {
+            return $this->json(
+                ['error' => 'Erreur lors de l’appel à OpenFoodFacts.', 'status' => $response->getStatusCode()],
+                Response::HTTP_BAD_GATEWAY
+            );
+        }
+
+        return $this->json($response->toArray());
+    }
+}
