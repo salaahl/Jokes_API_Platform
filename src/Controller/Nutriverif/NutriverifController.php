1cef9cc9b02908e10b33143bbc50d6625ed549a4
salaahl <sokhona.salaha@gmail.com>
2026-05-27 00:03:22 +0200

    fix: namespace

--- src/Controller/Nutriverif/NutriverifController.php
+++ src/Controller/Nutriverif/NutriverifController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller\Deepl;
+namespace App\Controller\Nutriverif;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
