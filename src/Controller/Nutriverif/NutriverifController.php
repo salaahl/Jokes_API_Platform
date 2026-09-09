b31935b0e532abde1a027556690b66803410f2ca
salaahl <sokhona.salaha@gmail.com>
2026-09-09 15:44:11 +0200

    fix

--- src/Controller/Nutriverif/NutriverifController.php
+++ src/Controller/Nutriverif/NutriverifController.php
@@ -152,8 +152,8 @@ class NutriverifController extends AbstractController
         // Modèles classés par ordre de priorité
         $models = [
             'gemini-3.8-flash',
-            'gemini-3.5-flash',
-            'gemini-2.5-flash'
+            'gemini-3.6-flash',
+            'gemini-3.5-flash'
         ];
 
         $payload = [
